0f3bc27083af100deef329348a25ad6169536f73
RedSquareBear <redsquarebear@users.noreply.github.com>
2022-03-07 21:02:36 +0000

    mixed lists

---
+++ mix-list/mix-list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+my_list = ["Bloodborne", "Dark Souls", "Sekiro", "Elden Ring", 10]
+
+print("The first game on our list is: " + my_list[0] )
+
+print("The most recent game on our list is: " + my_list[3] )
+
+print("It has received a review score from many publications of a " + str(my_list[4]) )
+
+print("The longest running series on this list is: " + my_list[1] )
+
+# This is just a list of my favorite FromSoftware games
