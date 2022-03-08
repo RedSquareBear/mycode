81044122dd35538ea2769edaeb97c4aab708cc8f
RedSquareBear <redsquarebear@users.noreply.github.com>
2022-03-08 17:21:05 +0000

    join and split

---
+++ strings/stringmethod01.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+lilstring = "Alta3 Research offers classes on coding"
+newlist = lilstring.split(" ")
+print(newlist)
+
+myiplist = ["192", "168", "1", "1"]
+
+singleip = ".".join(myiplist)
+
+print(singleip)
+
---
+++ strings/stringmethod02.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def main():
+
+    lilstring = "Alta3 Research offers classes on Python Coding"
+
+    newlist = lilstring.split(" ")
+    print(newlist)
+
+    myiplist = ["192", "168", "1", "1"]
+
+    singleip = ".".join(myiplist)
+    print(singleip)
+
+main()
+
