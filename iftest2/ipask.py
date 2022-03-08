9828a707102e772151ead9d36d87ad46e38146e8
RedSquareBear <redsquarebear@users.noreply.github.com>
2022-03-08 20:01:21 +0000

    if and else statements

---
+++ iftest2/ipask.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3 
+
+ipchk = input("Apply an IP address: ")
+
+if ipchk:
+    print("Looks like the IP address was set to: " + ipchk)
+else:
+    print("You did not provide an accurate input. Please try again.")
